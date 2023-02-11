97b249a300bf969a7954f2547a1e2ea83d6226a5
mzabrisk <mattzabriskie@matts-air.lan>
2023-02-10 22:21:17 -0700

    cleaned data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,23 @@
-let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	accessToken: API_KEY
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
 });
 
-let map = L.map('mapid', {
-	center: [25, 0],
-	zoom: 3,
-	layers: [outdoors]
-});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
-let stravaData = "https://raw.githubusercontent.com/mzabrisk/strava/main/strava_data.json"
+let stravaData = "https://raw.githubusercontent.com/mzabrisk/strava/main/strava_data2.json"
 d3.json(stravaData).then(function(data) {
-    console.log(data)
+    console.log(data.features[0])
 
-    run = []
-
-    for (i=0; i < 100; i++) {
-        console.log(data.type[i])
-        if (data.type[i] === 'Run') {
-            run.push(data[i])
-
-
-        }
-
-    }
-    console.log(run)
-    console.log(data.end_latlng)
 
 
     // L.geoJson(data).addTo(map)
